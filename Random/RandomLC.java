fd4909ee34c2d771b354912a31d9e86e4cd936e4
Zhijie Wang <120044311+zwang1115@users.noreply.github.com>
2023-05-31 23:02:42 +0800

    Add files via upload

---
+++ Random/RandomLC.java
@@ -0,0 +1,19 @@
+package Random;
+
+import java.util.Queue;
+
+public class RandomLC {
+
+/**
+ * Code not shown here please go check LeetCode
+ * LeetCode 385
+ * LeetCode 398
+ * LeetCode 382
+ * LeetCode 380
+ * LeetCode 381
+ * LeetCode 138
+ */
+
+
+
+}
